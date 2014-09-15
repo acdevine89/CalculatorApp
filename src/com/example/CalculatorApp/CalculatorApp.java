c1e5700b42a5dfd75d2d71f1e292614277a50073
Annie Devine <anniedevine@Annies-MacBook-Pro.local>
2014-09-14 21:29:52 -0400

    Creating main method

---
+++ src/com/example/CalculatorApp/CalculatorApp.java
@@ -0,0 +1,10 @@
+package com.example.CalculatorApp;
+import java.util.Scanner;
+
+public class CalculatorApp {
+    public static void main(String[] args) {
+    Scanner user = new Scanner(System.in);
+
+    int userChoice;
+    }
+}
