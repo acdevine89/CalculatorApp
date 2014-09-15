5d41fbb63cf521e4e9987b7db711f5117da60b65
Annie Devine <anniedevine@Annies-MacBook-Pro.local>
2014-09-14 21:47:04 -0400

    Creating double numberA and numberB for calculations

--- src/com/example/CalculatorApp/CalculatorApp.java
+++ src/com/example/CalculatorApp/CalculatorApp.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 public class CalculatorApp {
     public static void main(String[] args) {
     Scanner user = new Scanner(System.in);
-    boolean quit = false;
     int userChoice;
+    boolean quit = false;
+    double numberA;
+    double numberB;
     }
 }
