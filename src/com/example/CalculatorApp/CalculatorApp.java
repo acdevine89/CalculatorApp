a0f7b101c579dd3f18636daf2b6b44ccc93e8609
Annie Devine <anniedevine@Annies-MacBook-Pro.local>
2014-09-14 21:58:05 -0400

    Adding print statements to prompt user, and user.nextInt for userChoice

--- src/com/example/CalculatorApp/CalculatorApp.java
+++ src/com/example/CalculatorApp/CalculatorApp.java
@@ -10,7 +10,16 @@ public class CalculatorApp {
     double numberB;
 
     do {
-
+        System.out.println("Calculator Menu: What would you like to do?");
+        System.out.println("1 - Addition");
+        System.out.println("2 - Subtraction");
+        System.out.println("3 - Multiplication");
+        System.out.println("4 - Division");
+        System.out.println("5 - Square Root");
+        System.out.println("6 - Quit Program");
+        System.out.println("Enter menu selection by typing number here: ");
+        userChoice = user.nextInt();
+        
 
     } while(!quit);
 
